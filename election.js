de2f64bd0a70f1bcca99be30d9dd593042d4ca3c
Scott <scotthuston1982@gmail.com>
2018-02-14 14:56:11 -0500

    finished

--- election.js
+++ election.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var list = document.querySelector('#list');
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    data_type: 'JSON'
+  }).done(function(data){
+    // debugger
+    for(var i = 0; i < data.candidates.length; i++){
+      var li = document.createElement('li');
+      li.append(data.candidates[i].name);
+      li.append(" Votes: " + data.candidates[i].votes);
+      list.append(li);
+    }
+
+  });
 
 });
